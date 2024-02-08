195d0b9dd9441a22d7b9eba6f9cf0bfa76ee9233
Gabriel Becker da Fonseca <gbeckerdafonseca@gmail.com>
2024-02-08 10:22:20 -0300

    Criação do header da página homeNoAuth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import "../styles/globals/scss";
+import "../styles/globals.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
