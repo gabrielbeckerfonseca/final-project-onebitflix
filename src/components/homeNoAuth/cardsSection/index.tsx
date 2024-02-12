553f9446d10c10f1933f36f1e8e83e5a0b29a3cf
Gabriel Becker da Fonseca <gbeckerdafonseca@gmail.com>
2024-02-12 17:41:21 -0300

    Criação do footer

--- src/components/homeNoAuth/cardsSection/index.tsx
+++ src/components/homeNoAuth/cardsSection/index.tsx
@@ -5,7 +5,7 @@ const CardsSection = function () {
     return (
         <>
             <p className={styles.sectionTitle}>O QUE VOCÊ VAI ACESSAR</p>
-            <Container className="d-flex flex-wrap justify-content-center gap-4 pb-5">
+            <Container className="d-flex flex-wrap justify-content-center gap-4 py-4">
                 <div className={styles.card1}>
                     <p className={styles.cardTitle}>FRONT-END</p>
                     <p className={styles.cardDescription}>
