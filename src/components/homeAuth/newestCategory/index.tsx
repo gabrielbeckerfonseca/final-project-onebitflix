9dae1c02f20f06a49182b8266603aa9cc300073e
Gabriel Becker da Fonseca <gbeckerdafonseca@gmail.com>
2024-02-14 10:28:54 -0300

    Criação da seção de slides de lançamentos

---
+++ src/components/homeAuth/newestCategory/index.tsx
@@ -0,0 +1,26 @@
+import SlideComponent from "../../common/slideComponent";
+import courseService from "../../../services/courseService";
+import useSWR from "swr";
+import styles from "../../../../styles/slidecategory.module.scss"
+
+const NewestCategory = function () {
+
+    const { data, error } = useSWR("/newest", courseService.getNewestCourses);
+
+    if (error) return error;
+    if (!data) return (
+        <>
+            <p>Loading...</p>
+        </>
+    );
+
+
+    return (
+        <>
+            <p className={styles.titleCategory}>LANÇAMENTOS</p>
+            <SlideComponent course={data.data}/>
+        </>
+    );
+};
+
+export default NewestCategory;
