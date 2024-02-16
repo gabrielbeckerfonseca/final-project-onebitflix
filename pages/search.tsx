ce69704716232254af13adb9ddf714f15f53f587
Gabriel Becker da Fonseca <gbeckerdafonseca@gmail.com>
2024-02-16 01:09:17 -0300

    Criação dos elementos da página search e do seu visual

--- pages/search.tsx
+++ pages/search.tsx
@@ -4,6 +4,9 @@ import styles from "../styles/search.module.scss"
 import Head from "next/head"
 import { useEffect, useState } from "react";
 import courseService, { CourseType } from "../src/services/courseService";
+import { Container } from "reactstrap";
+import SearchCard from "../src/components/searchCard";
+import Footer from "../src/components/common/footer";
 
 const Search = function () {
     const router = useRouter();
@@ -26,13 +29,24 @@ const Search = function () {
                 <title>Onebitflix - {searchName}</title>
                 <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
             </Head>
-            <main>
-                <HeaderAuth />
-                {searchResult?.map((course) => (
-                    <div key={course.id}>
-                        <p>{course.name}</p>
-                    </div>
-                ))}
+            <main className={styles.main}>
+                <div className={styles.headFooterBg}>
+                    <HeaderAuth />
+                </div>
+                {searchResult.length >= 1 ? (
+                    <div className={styles.searchResult}>
+                        <Container className="d-flex flex-wrap justify-content-center gap-5 py-4">
+                            {searchResult?.map((course) => (
+                                <SearchCard key={course.id} course={course} />
+                            ))}
+                        </Container>
+                    </div>                    
+                ) : (
+                    <p className={styles.noSearchResult}>Nenhum resultado encontrado</p>
+                )}
+                <div>
+                    <Footer />
+                </div>
             </main>
         </>
     );
