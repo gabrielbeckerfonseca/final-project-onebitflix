c51a83f18009d58ca3bfc2b45b572ea7b7ff9de0
Gabriel Becker da Fonseca <gbeckerdafonseca@gmail.com>
2024-02-16 11:56:02 -0300

    Criação do método de pegar o objeto de um curso, comunicação com o front

--- pages/course/[id].tsx
+++ pages/course/[id].tsx
@@ -1,16 +1,38 @@
 import Head from "next/head";
 import styles from "../../styles/coursePage.module.scss";
 import HeaderAuth from "../../src/components/common/headerAuth";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import courseService, { CourseType } from "../../src/services/courseService";
 
-const CoursePage = function {
+const CoursePage = function () {
+    const [course, setCourse] = useState<CourseType>();
+    const router = useRouter();
+    const { id } = router.query;
+
+    const getCourse = async function () {
+        if (typeof id !== "string") return;
+
+        const res = await courseService.getEpisodes(id);
+
+        if (res.status === 200) {
+            setCourse(res.data);
+        };
+    };
+
+    useEffect(() => {
+            getCourse();
+    }, [id]);
+
     return (
         <>
             <Head>
-                <title>Onebitflix - {"nomeDoCurso"}</title>
+                <title>Onebitflix - {course?.name}</title>
                 <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
             </Head>
             <main>
                 <HeaderAuth />
+                <p>{course?.name}</p>
             </main>
         </>
     );
