8518c5f78a3d0328aaafc8854c8322c451bcb750
Gabriel Becker da Fonseca <gbeckerdafonseca@gmail.com>
2024-02-17 12:23:21 -0300

    Criação do método de tempo do episódio

--- pages/course/episode/[id].tsx
+++ pages/course/episode/[id].tsx
@@ -2,19 +2,58 @@ import Head from "next/head";
 import styles from "../../../styles/episodePlayer.module.scss";
 import { useRouter } from "next/router";
 import HeaderGeneric from "../../../src/components/common/headerGeneric";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import courseService from "../../../src/services/courseService";
 import PageSpinner from "../../../src/components/common/spinner";
 import { Button, Container } from "reactstrap";
 import ReactPlayer from "react-player";
+import Footer from "../../../src/components/common/footer";
+import watchEpisodeService from "../../../src/services/episodeService";
 
 const EpisodePlayer = function () {
     const router = useRouter();
     const [course, setCourse] = useState();
+    const [isReady, setIsReady] = useState(false);
     const episodeOrder = parseFloat(router.query.id?.toString() || "");
+    const episodeId = parseFloat(router.query.episodeid?.toString() || "");
     const courseId = router.query.courseid?.toString() || "";
 
-    const getCourse = async function (){
+    const [getEpisodeTime, setGetEpisodeTime] = useState(0);
+    const [episodeTime, setEpisodeTime] = useState(0);
+
+    const playerRef = useRef<ReactPlayer>(null)
+
+    const handleGetEpisodeTime = async () => {
+        const res = await watchEpisodeService.getWatchTime(episodeId);
+        console.log(res);
+        if (res.data !== null){
+            setGetEpisodeTime(res.data.seconds)
+        };
+    };
+
+    const handleSetEpisodeTime = async () => {
+        await watchEpisodeService.setWatchTime({
+            episodeId: episodeId,
+            seconds: Math.round(episodeTime),
+        });
+    };    
+
+    useEffect(() => {
+        handleGetEpisodeTime();
+    }, [router]);
+
+    const handlePlayerTime = () => {
+        playerRef.current?.seekTo(getEpisodeTime)
+        setIsReady(true);
+    };
+
+    if ( isReady === true ) {
+        setTimeout(() => {
+            handleSetEpisodeTime();
+        }, 1000 * 3)
+    };
+ 
+    const getCourse = async function () {
         if (typeof courseId !== "string") return;
 
         const res = await courseService.getEpisodes(courseId);
@@ -25,18 +64,31 @@ const EpisodePlayer = function () {
     };
 
     const handleLastEpisode = () => {
-        router.push(`/course/episode/${episodeOrder - 1}?courseid=${course.id}`)
-    }
+        router.push(`/course/episode/${episodeOrder - 1}?courseid=${course?.id}&episodeid=${
+            episodeId - 1
+            }`
+        );
+    };
 
     const handleNextEpisode = () => {
-        router.push(`/course/episode/${episodeOrder + 1}?courseid=${course.id}`)
-    }
+        router.push(`/course/episode/${episodeOrder + 1}?courseid=${course?.id}&episodeid=${
+            episodeId + 1
+            }`
+        );
+    };
 
     useEffect(() => {
         getCourse();
     }, [courseId]);
 
     if (course?.episodes === undefined) return <PageSpinner />;
+
+    if(episodeOrder + 1 < course?.episodes?.length) {
+        if(Math.round(episodeTime) === course.episodes[episodeOrder].secondsLong){
+            handleNextEpisode();
+        }
+
+    }
 
     return (
         <>
@@ -63,6 +115,11 @@ const EpisodePlayer = function () {
                             course.episodes[episodeOrder].videoUrl
                         }&token=${sessionStorage.getItem("onebitflix-token")}`}
                         controls
+                        ref={playerRef}
+                        onStart={handlePlayerTime}
+                        onProgress={(progress) => {
+                            setEpisodeTime(progress.playedSeconds);
+                        }}
                     />
                 )}
                 <div className={styles.episodeButtonDiv}>
@@ -93,6 +150,9 @@ const EpisodePlayer = function () {
                     {course.episodes[episodeOrder].synopsis}
                 </p>
             </Container>
+            <div style={{ backgroundColor: '#151515' }}>
+                <Footer />
+            </div>
         </main>
         </>
     );
